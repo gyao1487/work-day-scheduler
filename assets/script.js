d5c7661f5c9dfa1161344973120d249ea196763c
Grace <112779767+grachimus@users.noreply.github.com>
2022-10-31 12:51:58 -0500

    created timeblocks

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+//Display time and date at the top
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'));
+
+
+
+//Display
+    //if past then white
+    //if present then red
+    //if future then green
+
+
+//Save button
+    //Add event listener to save button
+const saveButton = document.getElementsByClassName("saveBtn")
+const input = document.getElementsByClassName ("description")
+    saveButton.addEventListener("click", )
+
+  
+    //Save to local storage as key value pair (time,task)
+
+//Refresh or open page
+    // saved events are rendered 
