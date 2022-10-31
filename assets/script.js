16f7d9fc0b881a5867f128f78c582de02b55c65f
Grace <112779767+grachimus@users.noreply.github.com>
2022-10-31 17:56:30 -0500

    Color coding WIP

--- assets/script.js
+++ assets/script.js
@@ -4,19 +4,37 @@ $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm a'));
 
 
 //Display
-    //if past then white
-    //if present then red
-    //if future then green
+/*function colorCode ()
+    let currentTime = moment().hours();
+    const timeblock = document.getelementsbyclass('time-block')
+    let timeblockTime = timeblock. */
+const colorcode = document.getElementsByClassName("colorcode");
+let currentHour = moment().hours(); 
+
+
+//Change color depending on current time
+Array.from(colorcode).forEach(element => {
+    let colorcodeHour = parseInt(colorcode.id);
+    if (currentHour === colorcodeHour) {
+        colorcode.setAttribute('class','present')
+        } else if (currentHour > colorcodeHour) {
+        colorcode.setAttribute('class','future') //This is supposed to turn the textarea green as a test (since it is past 5pm rn)
+        } else if (currentHour < colorcodeHour) {
+        colorcodedDiv.setAttribute('class','future')
+        }
+    }
+)
 
 
 //Save button
     //Add event listener to save button
 const saveButton = document.getElementsByClassName("saveBtn")
 const input = document.getElementsByClassName ("description")
-    saveButton.addEventListener("click", )
+
+//saveButton.addEventListener("click", )
 
   
     //Save to local storage as key value pair (time,task)
 
-//Refresh or open page
-    // saved events are rendered 
+//Open Page
+    // Saved events are rendered
